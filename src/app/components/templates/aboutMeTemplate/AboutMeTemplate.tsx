a20eead41a60fcb080ad4320566d11d0c5287736
David Barón <103337874+Ldbelop@users.noreply.github.com>
2023-10-11 22:01:16 -0500

    Se finaliza en landscape la sección de about-me (about me)
    Falta implementar los sliders de cada habilidad

--- src/app/components/templates/aboutMeTemplate/AboutMeTemplate.tsx
+++ src/app/components/templates/aboutMeTemplate/AboutMeTemplate.tsx
@@ -5,12 +5,23 @@ import AboutImage from '@/app/components/atoms/aboutImage/AboutImage'
 
 const AboutMeTemplate = () => {
     return (
-        <section className={styles.container} id='About-me'>
-            <SectionTitle titleText={'About me'}/>
-            <JobDescription descText='Nothing is built alone, improve with others'/>
-            <AboutImage />
-            <JobDescription descText='I look forward to be a high impact developer, but meanwhile I focus on teaching, learning and improving my skills & habits.'/>
-            <JobDescription descText='My name is David Barón, I am a 18 year old Front-end developer, but im also a compulsive builder, from legos to web apps, I love building things!'/>
+        <section className={`${styles.container}`} id='About-me'>
+            <div className={styles.mobile}>
+                <SectionTitle titleText={'About me'}/>
+                <JobDescription descText='Nothing is built alone, improve with others'/>
+                <AboutImage />
+                <JobDescription descText='I look forward to be a high impact developer, but meanwhile I focus on teaching, learning and improving my skills & habits.'/>
+                <JobDescription descText='My name is David Barón, I am a 18 year old Front-end developer, but im also a compulsive builder, from legos to web apps, I love building things!'/>
+            </div>
+            <div className={styles.landscape}>
+                <div>
+                    <SectionTitle titleText={'About me'}/>
+                    <JobDescription descText='Nothing is built alone, improve with others'/>
+                    <JobDescription descText='I look forward to be a high impact developer, but meanwhile I focus on teaching, learning and improving my skills & habits.'/>
+                    <JobDescription descText='My name is David Barón, I am a 18 year old Front-end developer, but im also a compulsive builder, from legos to web apps, I love building things!'/>
+                </div>
+                <AboutImage />
+            </div>
         </section>
     )
 }
