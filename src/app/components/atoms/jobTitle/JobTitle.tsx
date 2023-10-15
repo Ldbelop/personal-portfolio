31a005c7af59aa65feb84d74aa19706137f5b9bb
David Barón <103337874+Ldbelop@users.noreply.github.com>
2023-10-14 20:58:47 -0500

    Fixed Font tomfoolery again (about me)

--- src/app/components/atoms/jobTitle/JobTitle.tsx
+++ src/app/components/atoms/jobTitle/JobTitle.tsx
@@ -1,9 +1,9 @@
 import styles from './jobTitle.module.css'
-import { poppinsFont } from '@/utils/fonts'
+import { poppins } from '@/utils/fonts'
 
 const JobTitle = ({ titleText, place }: { titleText: string, place: string }) => {
   return (
-    <h2 className={`${styles.title} ${poppinsFont.className}`}>
+    <h2 className={`${styles.title} ${poppins.className}`}>
       {titleText} <span>at {place}</span>
     </h2>
   )
