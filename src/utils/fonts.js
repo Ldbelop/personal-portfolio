d60c0001cfd6b3e8497f7037b7c164cc4e13bc13
David Barón <103337874+Ldbelop@users.noreply.github.com>
2023-10-16 00:02:22 -0500

    Revert "Fixed fonts tomfoolery and image align in mobile (about-me)"
    This reverts commit a38b1e10f7310e8db1581bcc0fe1d80bfa8d7ceb.

--- src/utils/fonts.js
+++ src/utils/fonts.js
@@ -3,15 +3,18 @@ import { Inter, Poppins, Calistoga } from 'next/font/google'
 export const inter = Inter({
   weight: ['400','500'],
   subsets: ['latin'],
+  variable: '--inter-font'
 })
 
-export const poppins = Poppins({
+export const poppinsFont = Poppins({
   weight: ['400','500','600'],
   style: ['normal', 'italic'],
   subsets: ['latin'],
+  variable: '--poppins-font'
 })
 
 export const calistoga = Calistoga({
   weight: ['400'],
   subsets: ['latin'],
+  variable: '--calistoga-font'
 })
