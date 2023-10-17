7cbafc3d9c7e8bd9be8eabd89e758e891380c078
David Barón <103337874+Ldbelop@users.noreply.github.com>
2023-10-17 10:43:24 -0500

    Finished views up to portfolio
    Falta hacer el menú desplegable

--- src/app/components/atoms/technology/Technology.tsx
+++ src/app/components/atoms/technology/Technology.tsx
@@ -3,10 +3,10 @@ import styles from './technology.module.css'
 
 const Technology = ({ children, techName }: { children: React.ReactNode, techName: string  }) => {
   return (
-    <>
+    <div className={styles.container}>
       {children}
       <h4 className={`${styles.text} ${poppins.className}`}>{techName}</h4>
-    </>
+    </div>
   )
 }
 
